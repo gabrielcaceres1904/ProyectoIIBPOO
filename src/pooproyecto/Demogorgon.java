42fb263c4e7e7521e96a9eac395f61be2a5d727c
justinyamberla <55857299+justinyamberla@users.noreply.github.com>
2020-09-01 16:51:23 -0500

    Enemigos
    desarrollo-enemigos actualización

--- src/pooproyecto/Demogorgon.java
+++ src/pooproyecto/Demogorgon.java
@@ -3,28 +3,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyectoiib_poo;
+package pooproyecto;
 
 /**
  *
  * @author justi
  */
 public class Demogorgon extends Enemigo{
-    private int vida = 150;
+    private int vida = (int) Math.random()*(200-150+1)+150;
     
     
     @Override
     public int ataque(){
-        int dañoCausado = (int)(Math.random()*3);
+        int dañoCausado = (int)(Math.random()*(7-3+1)+3);
         return dañoCausado;
     }
     
     @Override
     public int ataqueEspecial(){
-        int dañoCausado = (int)(Math.random()*3);
+        int dañoCausado = (int)(Math.random()*(7-3+1)+3);
         return dañoCausado;
     }
-    
 
     public int getVida() {
         return vida;
