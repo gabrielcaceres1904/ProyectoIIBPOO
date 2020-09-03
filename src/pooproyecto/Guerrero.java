4fdd39b6375d6f20e944f2963585a044284ef08b
tapiaalexv <55813396+tapiaalexv@users.noreply.github.com>
2020-09-03 16:51:25 -0500

    Creacion de todos los heroes
    Se creo todas las clases de los herores  a continucacion quedaria configurar la GUI

---
+++ src/pooproyecto/Guerrero.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pooproyecto;
+
+/**
+ *
+ * @author tapia
+ */
+public class Guerrero extends Heroes{
+    
+    public void setVida(int vida) {//30
+        this.vida = vida;
+    }
+   
+    public void setRecurso(int recurso) { //estamina -> 100
+        this.recurso = recurso;
+    }
+
+    @Override
+    int atk() {
+        recurso -= 10;
+        return super.damage = (int) ((Math.random()*15)+3);
+    }
+
+    @Override
+    int atkEsp() {
+        recurso -= 20; 
+        return super.damage = (int) ((Math.random()*30)+7);
+    }
+    
+}
