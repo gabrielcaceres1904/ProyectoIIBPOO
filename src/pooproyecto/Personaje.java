69c0fbd24c563060f74a6bea8bd359ee4327cef8
tapiaalexv <55813396+tapiaalexv@users.noreply.github.com>
2020-09-03 14:48:04 -0500

    Clase Personaje
    Creacion de clase padre

---
+++ src/pooproyecto/Personaje.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pooproyecto;
+
+/**
+ *
+ * @author tapia
+ */
+public abstract class Personaje {
+    
+    protected int vida;
+    protected int damage;
+    
+    abstract int atk();
+    abstract int atkEsp();
+    
+    
+    
+}
